b2519fb2779b2f74c27ef575d43b2787c4bf38c6
Michael Liberatore <mi@chael.co>
2020-08-14 12:09:33 -0400

    Initial Commit

---
+++ Provider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Provider
+//
+//  Created by Michael Liberatore on 8/14/20.
+//  Copyright © 2020 Lickability. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

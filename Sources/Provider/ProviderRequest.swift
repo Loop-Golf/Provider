d22f127b126cee4f9356d073bfa4da3d7110e81a
Twigz <aharris.1990@gmail.com>
2020-08-28 15:09:13 -0400

    Adds docs for ProviderRequest

--- Sources/Provider/ProviderRequest.swift
+++ Sources/Provider/ProviderRequest.swift
@@ -8,6 +8,9 @@
 
 import Networking
 
+/// A protocol that defines the parameters that make up a item providing request.
 public protocol ProviderRequest: NetworkRequest {
+    
+    /// The key to use for persistence of the item.
     var persistenceKey: Key { get }
 }

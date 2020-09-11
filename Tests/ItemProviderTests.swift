4248ab5c2f9d4013b7532f6b6db15ce12f1ff84b
Twigz <aharris.1990@gmail.com>
2020-09-11 14:37:47 -0400

    add cached results test

--- Tests/ItemProviderTests.swift
+++ Tests/ItemProviderTests.swift
@@ -228,6 +228,36 @@ class ItemProviderTests: XCTestCase {
         wait(for: [expectation], timeout: 2)
     }
     
+    func testProvideItemPublisherReturnsCachedResult() {
+        let request = TestProviderRequest()
+        
+        let expectation = self.expectation(description: "The item will exist.")
+        
+        let originalStub = stub(condition: { _ in true }) { _ in
+            fixture(filePath: OHPathForFile("Item.json", type(of: self))!, headers: nil)
+        }
+        
+        provider.provide(request: request, providerBehaviors: [], requestBehaviors: [])
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+            .handleEvents(receiveOutput: { (_: TestItem) in
+                HTTPStubs.removeStub(originalStub)
+                
+                stub(condition: { _ in true }) { _ in
+                    fixture(filePath: OHPathForFile("InvalidItem.json", type(of: self))!, headers: nil)
+                }
+            })
+            .flatMap { _ -> AnyPublisher<TestItem, ProviderError> in
+                self.provider.provide(request: request, providerBehaviors: [], requestBehaviors: [])
+            }
+            .sink(receiveCompletion: { _ in }, receiveValue: { (item: TestItem) in
+                expectation.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 2)
+    }
+    
     func testProvideItemsPublisher() {
         let request = TestProviderRequest()
         
@@ -263,6 +293,37 @@ class ItemProviderTests: XCTestCase {
             .eraseToAnyPublisher()
             .sink(receiveCompletion: { _ in expectation.fulfill() }, receiveValue: { (items: [TestItem]) in
                 XCTFail("There should be no items.")
+                expectation.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 2)
+    }
+    
+    func testProvideItemsPublisherReturnsCachedResult() {
+        let request = TestProviderRequest()
+        
+        let expectation = self.expectation(description: "The item will exist.")
+        
+        let originalStub = stub(condition: { _ in true }) { _ in
+            fixture(filePath: OHPathForFile("Items.json", type(of: self))!, headers: nil)
+        }
+        
+        provider.provideItems(request: request, providerBehaviors: [], requestBehaviors: [])
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+            .handleEvents(receiveOutput: { (_: [TestItem]) in
+                HTTPStubs.removeStub(originalStub)
+                
+                stub(condition: { _ in true }) { _ in
+                    fixture(filePath: OHPathForFile("InvalidItems.json", type(of: self))!, headers: nil)
+                }
+            })
+            .flatMap { _ -> AnyPublisher<[TestItem], ProviderError> in
+                self.provider.provideItems(request: request, providerBehaviors: [], requestBehaviors: [])
+            }
+            .sink(receiveCompletion: { _ in }, receiveValue: { (items: [TestItem]) in
+                XCTAssertEqual(items.count, 3)
                 expectation.fulfill()
             })
             .store(in: &cancellables)
